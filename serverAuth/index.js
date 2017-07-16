ac6f3f3b5d4d6742a055fe9583122a059f5c2baf
Ian8829 <ghjkghj@naver.com>
2017-07-16 17:46:56 +0900

    More Server Setup

--- serverAuth/index.js
+++ serverAuth/index.js
@@ -1,1 +1,16 @@
 // Main starting point
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+// App Setup
+
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server is litegning on: ${port}`);
