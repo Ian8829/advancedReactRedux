5a367822f3bef0943981acff48c6cfd86aecfea8
Ian8829 <ghjkghj@naver.com>
2017-07-16 17:57:52 +0900

    Express middleware, babel setting, nodemon

--- serverAuth/index.js
+++ serverAuth/index.js
@@ -7,10 +7,11 @@ import morgan from 'morgan';
 const app = express();
 
 // App Setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log(`Server is litegning on: ${port}`);
+console.log(`Server is listening on: ${port}`);
