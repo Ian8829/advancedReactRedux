1e9fee759c1d8c1b514c76fdfd8a3c5d93641f43
Ian8829 <ghjkghj@naver.com>
2017-07-16 15:33:46 +0900

    HOC Scaffold Code

--- hoc/src/components/require_authentication.js
+++ hoc/src/components/require_authentication.js
@@ -4,7 +4,6 @@ export default function (ComposedComponent) {
   class Authentication extends Component {
     render() {
       return(
-        console.log(this.props.resources) // => resourceList
         <ComposedComponent {...this.props}/>
       );
     }
@@ -13,12 +12,3 @@ export default function (ComposedComponent) {
   return Authentication;
 }
 
-// In some other location ... Not in this file...
-// We want to use this HOC
-import Authentication; // Thie is my HOC
-import Resources; // This is the component I want to wrap
-
-const ComposedComponent = Authentication(Resources);
-
-// In some render method...
-<ComposedComponent resources={resourceList} />
