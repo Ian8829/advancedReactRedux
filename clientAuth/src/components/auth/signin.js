39a6b09c832cc95ffd8544fb3e31cd21a76f926a
taeshik Hwang <ghjkghj@naver.com>
2017-07-17 14:39:38 +0900

    Scaffolding the Signin Form

---
+++ clientAuth/src/components/auth/signin.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class Signin extends Component {
+  render() {
+    return(
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input className="form-control"/>
+        </fieldset>
+        <button className="btn btn-primary" action="submit">
+          Sign in
+        </button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+})(Signin);
