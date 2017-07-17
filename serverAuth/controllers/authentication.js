c36146bd86557ffffb37f1210c36969b7a183ba6
taeshik Hwang <ghjkghj@naver.com>
2017-07-17 10:48:51 +0900

    Creating user Records

--- serverAuth/controllers/authentication.js
+++ serverAuth/controllers/authentication.js
@@ -6,12 +6,24 @@ exports.signup = (req, res, next) => {
 
   // See if a user with the given email exists
   User.findOne({ email }, (err, existingUser) => {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
   });
-
-  // If a user with email does exist, return an error
-
-  // If a user with email does NOT exist, create and save user record
-
-  // Respond to request indicating the user was created
 };
